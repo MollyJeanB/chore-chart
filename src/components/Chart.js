0a9f6243e7b1ce1a58d3f1f987623fc2ac7b2dc6
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-03-06 08:50:49 -0800

    initial commit

---
+++ src/components/Chart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import ChoreList from "./chore-list";
+
+export default function Chart() {
+  return (
+    <div className="chart-contain">
+      <ChoreList />
+    </div>
+  );
+}
